2a8f368520046511212318f3026e678cad395993
lptMint <hankgit-h@hotmail.com>
2025-10-18 10:35:20 -0500

    enable excel file operations in main and expose functions in __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,12 @@
 from .getdata import get_data
 from .search_file import find_excel_file
+from .excel_manage import open_excel, remove_columns, search_information
 
 
-__all__ = ['get_data', 'find_excel_file']
+__all__ = [
+    'get_data', 
+    'find_excel_file', 
+    'open_excel',
+    'remove_columns',
+    'search_information'
+]
