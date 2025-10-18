079e1acd25e7dedd0170595d373c8adc82a48bf8
lptMint <hankgit-h@hotmail.com>
2025-10-18 18:56:50 -0500

    feat: integrate dynamic column selection using get_columns and load_and_clean_excel in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from src import get_data
 from src import find_excel_file
-from src import open_excel, remove_columns, search_information
+from src import open_excel, get_columns, load_and_clean_excel, remove_columns, search_information
 import time
 
 def main():
@@ -12,25 +12,50 @@ def main():
     if find_excel_file(file, './'):
         print(f'\nFile "{file}", found and ready to use.\n')
         open_excel(file, 'Consolidado')
-    
+        
     else:
         print(f'\nFile "{file}", not found in system.\n')
         return 
 
+    columns = get_columns()
+    load_and_clean_excel(columns)
+
     user_selection = get_data(
-        'Wath do you wish to perform:\n1. Perfom search \n2. Remove columns\n>'
+        'Wath do you wish to perform:\n1. Remove columns \n2. Perfom search\n>'
     )
 
     if user_selection == '1':
+        print('Select the column to delete')
+        time.sleep(1.5)
+        for column, index in enumerate(columns):
+            print(f'{index}. {column}')
+
+        drop_columns = []
+        while True:
+            select_columns= get_data(
+                'Column to be deleted\n'
+                'Cancel\n'
+            )
+
+            if select_columns == 'cancel':
+                break
+
+            for column in select_columns.split():
+                drop_columns.append(columns[int(column)])
+        
+        if len(drop_columns) > 0:
+            print('\nRemoving columns ...\n')
+            time.sleep(1)
+            remove_columns(drop_columns)
+
+    if user_selection == '2':
         print('Perfom search')
-        document = get_data('Document Number')
-        search_information(document)
+        label = get_data('\nName of the label to be selected')
 
-
-    elif user_selection == '2':
-        print('Remove colummns')
-        remove_columns()
-
+        if label == 'Patient_Document_Number':
+            dni = get_data('\nDocument')
+            search_information(label, dni)
+  
     else:
         print('Invalid Option')
 
