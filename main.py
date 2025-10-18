82df24697c1b062879a884b9efc21bc96e569391
lptMint <hankgit-h@hotmail.com>
2025-10-18 10:33:48 -0500

    enable excel file interaction via excel_manage module

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 from src import get_data
+from src import find_excel_file
+from src import open_excel, remove_columns, search_information
 import time
 
 def main():
     file = get_data('File name')
 
-    time.sleep(1.5)
+    time.sleep(1)
 
-    print(f'\nFile "{file}" found and ready to use.\n')
+    if find_excel_file(file, './'):
+        print(f'\nFile "{file}", found and ready to use.\n')
+        open_excel(file, 'Consolidado')
+    
+    else:
+        print(f'\nFile "{file}", not found in system.\n')
+        return 
 
     user_selection = get_data(
         'Wath do you wish to perform:\n1. Perfom search \n2. Remove columns\n>'
@@ -15,9 +23,13 @@ def main():
 
     if user_selection == '1':
         print('Perfom search')
-    
+        document = get_data('Document Number')
+        search_information(document)
+
+
     elif user_selection == '2':
         print('Remove colummns')
+        remove_columns()
 
     else:
         print('Invalid Option')
