9c85fb8a920c9ae85128e72afe2fef2f96434a1a
lptMint <hankgit-h@hotmail.com>
2025-10-18 18:46:09 -0500

    fead: add get_columns and load_and_clean_excel functions and improve comments in search_information

--- src/excel_manage.py
+++ src/excel_manage.py
@@ -8,18 +8,23 @@ def open_excel(filename, sheet):
     file_name = filename
     df = pd.read_excel(filename, sheet)
 
-drop_columns = [
-    'column_one',
-    'column_two',
-    'column_three',
-    'column_four',
-    'column_five'
-]
+def get_columns():
+    all_columns = (df.columns.values).tolist()
+    return all_columns
 
-def remove_columns():
+def load_and_clean_excel(columns):
+    for column_name in columns:
+        # Ensure that the 'label' column is a string and clean values such as '.0'
+        if df[column_name].dtype != 'object':
+            df[column_name] = df[column_name].astype(str)
+
+        df[column_name] = df[column_name].str.replace('.0', '', regex=False)
+
+def remove_columns(drop_columns):
     df.drop(columns=drop_columns, inplace=True)
     print(df)
 
-def search_information(document):
-    patient = df.loc[df['label'] == document]
+def search_information(label, value):
+    # Select rows where 'label' matchs the document
+    patient = df.loc[df[label] == value] 
     print(patient)
