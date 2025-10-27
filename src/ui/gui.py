325cd40762f6efd03c6749e1fb3a6383ad5199b6
jhon <hankgit-h@hotmail.com>
2025-10-27 07:08:46 -0500

    feat: integrate sidebar component into main interface

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,6 +1,14 @@
-from src.ui import ft
+import flet as ft
+from src.ui.components import create_sidebar
 
 def main_app(page: ft.Page):
-    print('run')
+    page.title = 'Consilidado'
+
+    page.add(
+        ft.Row(
+            controls=[create_sidebar()],
+            expand=True
+        )
+    )
 
 ft.app(main_app)
