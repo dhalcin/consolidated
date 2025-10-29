ad5862db8c9d196a8cbe9e7d377cf50a51157667
jhon <hankgit-h@hotmail.com>
2025-10-29 15:19:46 -0500

    feat: add header component and restructure layout into sections

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,12 +1,25 @@
 import flet as ft
 from src.ui.components import create_sidebar
+from src.ui.components import create_header
 
 def main_app(page: ft.Page):
     page.title = 'Consilidado'
 
+    main_content = ft.Column(
+        [
+            ft.Text('DataTable')
+        ]
+    )
+
+    page.appbar = create_header()
+
     page.add(
         ft.Row(
-            controls=[create_sidebar()],
+            [
+                create_sidebar(),
+                ft.VerticalDivider(width=1),
+                main_content
+            ],
             expand=True
         )
     )
