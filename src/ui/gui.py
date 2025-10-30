7c1d51395da08aa3445e7169c3ada26d02ee91d4
lptMint <hankgit-h@hotmail.com>
2025-10-30 01:21:07 -0500

    feat: display selected file name in header after file selection

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -6,16 +6,18 @@ from src.ui.components import create_file_pick_button
 def main_app(page: ft.Page):
     page.title = 'Consilidado'
 
+    select_file_text = ft.Text(value='', size=14, color=ft.Colors.BLACK)
+
     def handle_file_pick(e: ft.FilePickerResultEvent):
         if e.files:
             file_name = e.files[0].name
-            print(file_name)
+            select_file_text.value = f'Selected file : {file_name}'
+            page.update()
 
     file_picker_dialog = ft.FilePicker(on_result=handle_file_pick)
     page.overlay.append(file_picker_dialog)
 
-    page.update()
-
+    
     select_file_button = create_file_pick_button(
         file_picker_dialog.pick_files,
         'Choose files ...'
@@ -27,7 +29,9 @@ def main_app(page: ft.Page):
         ]
     )
 
-    page.appbar = create_header(select_file_button)
+    print(file_picker_dialog)
+
+    page.appbar = create_header(select_file_button, select_file_text)
 
     page.add(
         ft.Row(
