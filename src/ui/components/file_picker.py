ef32f208951842e1deb34b8c12465344c1d7af26
lptMint <hankgit-h@hotmail.com>
2025-10-30 00:31:32 -0500

    style: adjust button dimensions and styling for compact apparence

--- src/ui/components/file_picker.py
+++ src/ui/components/file_picker.py
@@ -5,5 +5,14 @@ def create_file_pick_button(pick_action: Callable, text: str):
     return ft.ElevatedButton(
         text,
         icon=ft.Icons.UPLOAD_FILE,
-        on_click=lambda e: pick_action()
+        on_click=lambda e: pick_action(),
+        bgcolor=ft.Colors.BLUE_400,
+        color=ft.Colors.WHITE,
+        height=32, 
+        width=170,
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=16),
+            padding=ft.padding.symmetric(horizontal=10, vertical=0),
+            elevation=1
+        )
     )
