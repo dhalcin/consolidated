e66dbf321c4f67a0808f153c1bda138f398c0f63
lptMint <hankgit-h@hotmail.com>
2025-10-30 00:39:22 -0500

    feat: integrate pick file component into main interface

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,9 +1,25 @@
 import flet as ft
 from src.ui.components import create_sidebar
 from src.ui.components import create_header
+from src.ui.components import create_file_pick_button
 
 def main_app(page: ft.Page):
     page.title = 'Consilidado'
+
+    def handle_file_pick(e: ft.FilePickerResultEvent):
+        if e.files:
+            file_name = e.files[0].name
+            print(file_name)
+
+    file_picker_dialog = ft.FilePicker(on_result=handle_file_pick)
+    page.overlay.append(file_picker_dialog)
+
+    page.update()
+
+    select_file_button = create_file_pick_button(
+        file_picker_dialog.pick_files,
+        'Choose files ...'
+    )
 
     main_content = ft.Column(
         [
@@ -11,7 +27,7 @@ def main_app(page: ft.Page):
         ]
     )
 
-    page.appbar = create_header()
+    page.appbar = create_header(select_file_button)
 
     page.add(
         ft.Row(
