4d4567ec18209963e2251971ab2230077c886daa
lptMint <hankgit-h@hotmail.com>
2025-10-30 00:33:32 -0500

    wrap file button in Container to prevent AppBar expansion and align vertically

--- src/ui/components/header.py
+++ src/ui/components/header.py
@@ -1,7 +1,14 @@
 import flet as ft
+from typing import Callable
 
-def create_header():
+def create_header(file_button: Callable):
     return ft.AppBar(
-        leading=ft.Icon(ft.Icons.UPLOAD_FILE),
-        bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST
+        leading=ft.Container(
+            content=file_button,
+            alignment=ft.alignment.center,
+            margin=ft.margin.only(left=10, top=4, bottom=4)
+        ),
+        leading_width=190,
+        bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+        toolbar_height=45
     )
