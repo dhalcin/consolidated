6d897e93d443e7f0078f8fe54ce263bc40335851
jhon <hankgit-h@hotmail.com>
2025-10-31 03:40:09 -0500

    refactor(gui): integrate get_columns and update the use of open_excel in the file selection flow

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -2,7 +2,7 @@ import flet as ft
 from src.ui.components import create_sidebar
 from src.ui.components import create_header
 from src.ui.components import create_file_pick_button
-from src.data_logic import open_excel
+from src.data_logic import open_excel, get_columns
 
 def main_app(page: ft.Page):
     page.title = 'Consolidado'
@@ -19,15 +19,18 @@ def main_app(page: ft.Page):
             page.update()
 
     def on_file_selected(path):
-        file_path = open_excel(path)
-        print(file_path)
+        df = open_excel(path)
+        all_columns(df)
+
+    def all_columns(df):
+        columns = get_columns(df)
+        print(columns)
 
     file_picker_dialog = ft.FilePicker(
         on_result=lambda e: handle_file_pick(e, on_file_selected)
     )
     page.overlay.append(file_picker_dialog)
 
-    
     select_file_button = create_file_pick_button(
         file_picker_dialog.pick_files,
         'Choose files ...'
