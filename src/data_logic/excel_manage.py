78e896df485374af96e3f8505c552713ddd715b0
jhon <hankgit-h@hotmail.com>
2025-10-31 03:39:05 -0500

    feat(excel_manage): add the get_columns function retrive the columns from the Excel file

--- src/data_logic/excel_manage.py
+++ src/data_logic/excel_manage.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 def open_excel(file_path):
     df = pd.read_excel(file_path, 'Consolidado')
-    print(df)
+    return df
+
+def get_columns(df):
+    all_columns = (df.columns.values).tolist()
+    return all_columns
