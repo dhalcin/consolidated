5881c143e0ba634cb1099e8d0e9e99c4b120c542
jhon <hankgit-h@hotmail.com>
2025-11-01 00:25:28 -0500

    feat(excel_manage): add the load_clean_excel function the so that columns have a clean format

--- src/data_logic/excel_manage.py
+++ src/data_logic/excel_manage.py
@@ -7,3 +7,11 @@ def open_excel(file_path):
 def get_columns(df):
     all_columns = (df.columns.values).tolist()
     return all_columns
+
+def load_and_clean_excel(df, columns):
+    for column_name in columns:
+        # Ensure that the 'label' column is a string and clean values such as '.0'
+        if df[column_name].dtype != 'object':
+            df[column_name] = df[column_name].astype(str)
+
+        df[column_name] = df[column_name].str.replace('.0', '', regex=False)
