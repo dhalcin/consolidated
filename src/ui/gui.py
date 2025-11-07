4ebcd48f68061ab6ae386bffba38551c714cd39e
jhon <hankgit-h@hotmail.com>
2025-11-06 23:07:08 -0500

    feat(ui/gui): add loader and refactor Excel loading for async/await

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,35 +1,41 @@
-import flet as ft
 from src.ui.components import create_sidebar
 from src.ui.components import create_header
 from src.ui.components import create_file_pick_button
 from src.data_logic import open_excel, get_columns, load_and_clean_excel
 from src.ui.views import create_table_view
+import flet as ft
+import asyncio
 
-def main_app(page: ft.Page):
+async def main_app(page: ft.Page):
     page.title = 'Consolidado'
-
+    
     select_file_text = ft.Text(value='', size=14, color=ft.Colors.BLACK)
-
-    def handle_file_pick(e: ft.FilePickerResultEvent, on_file_selected: callable):
-        global manage_file
+    async def handle_file_pick(e: ft.FilePickerResultEvent):
         if e.files:
             file_name = e.files[0].name
             file_path = e.files[0].path
             select_file_text.value = f'Selected file : {file_name}'
-            on_file_selected(file_path)
             page.update()
+            await on_file_selected(file_path)
 
+    loader = ft.ProgressBar(width=400, visible=False)
     view_columns_container = ft.Column()
-    def on_file_selected(file_path):
-        df = open_excel(file_path)
-        columns = get_columns(df)
-        load_and_clean_excel(df, columns) 
-        view_columns_container.controls = [create_table_view(columns)] # create_table_view is main_content
+
+    async def on_file_selected(file_path):
+        loader.visible = True
         page.update()
 
-    file_picker_dialog = ft.FilePicker(
-        on_result=lambda e: handle_file_pick(e, on_file_selected)
-    )
+        await asyncio.sleep(0.1)
+
+        df = await asyncio.to_thread(open_excel, file_path)
+        columns = await asyncio.to_thread(get_columns, df)
+        await asyncio.to_thread(load_and_clean_excel, df, columns)
+
+        view_columns_container.controls = [create_table_view(columns)]
+        loader.visible = False
+        page.update()
+
+    file_picker_dialog = ft.FilePicker(on_result=handle_file_pick)
     page.overlay.append(file_picker_dialog)
 
     select_file_button = create_file_pick_button(
@@ -44,10 +50,8 @@ def main_app(page: ft.Page):
             [
                 create_sidebar(),
                 ft.VerticalDivider(width=1),
-                view_columns_container
+                ft.Column([loader, view_columns_container], expand=True),
             ],
             expand=True
         )
     )
-
-ft.app(main_app)
