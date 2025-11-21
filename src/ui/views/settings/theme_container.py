9d77e2fe91f311ed0bcf184d6150e88267368adc
jhon <hankgit-h@hotmail.com>
2025-11-21 11:22:24 -0500

    feat(settings): create initial module for theme management

---
+++ src/ui/views/settings/theme_container.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def theme_switch():
+    return 
+
+def build_theme_container():
+    return ft.Row(
+        [
+            ft.Icon(ft.Icons.DARK_MODE)
+        ],
+        alignment=ft.MainAxisAlignment.END,
+        expand=True
+    )
