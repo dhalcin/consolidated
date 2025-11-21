38693dbdca0fc7f4f1b25fcb69fca075a6455b35
jhon <hankgit-h@hotmail.com>
2025-11-21 11:39:26 -0500

    feat(ui/gui): add forms container and export build_forms_container

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -1,5 +1,6 @@
 import flet as ft
 from src.ui.views.settings import build_theme_container
+from src.ui.views.forms import build_forms_container
 
 def main(page: ft.Page):
 
@@ -9,7 +10,12 @@ def main(page: ft.Page):
                 ft.Container(
                     content=build_theme_container()
                     # border=ft.border.all(1, ft.Colors.AMBER)
-                )
+                ),
+                ft.Container(
+                    content=build_forms_container(),
+                    expand=True,
+                    #border=ft.border.all(2, ft.Colors.YELLOW)
+                ),
             ],
             expand=True
         )
