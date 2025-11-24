bcdaa72f960f209a34269e842a624dce3f4ede8d
jhon <hankgit-h@hotmail.com>
2025-11-24 04:42:34 -0500

    feat(care_data): add initial structure for care data views

---
+++ src/ui/views/forms/sections/care_data_container.py
@@ -0,0 +1,41 @@
+import flet as ft
+
+def main_content():
+        return ft.Column(
+        [
+            ft.ResponsiveRow(
+                [
+                    ft.Container(
+                        content=ft.Text(value='Apellido Materno', color='red'),
+                        col={'sm': 6}
+                    ),
+                    ft.Container(
+                        content=ft.Text(value='Apellido Paterno', color='red'),
+                        col={'sm': 6}
+                    ),
+                    ft.Container(
+                        content=ft.Text(value='Nombres', color='red'),
+                        col={'sm': 6}
+                    ),
+                    ft.Container(
+                        content=ft.Text(value='DNI', color='red')
+                    )
+                ]
+            )
+        ]
+    )
+
+
+def build_care_data_container():
+    return ft.Column(   # Column is used to occupy the entire size of the parent Container that is in forms_container
+                        # a case that does not work with Row
+        [
+            ft.Row(
+                [ft.Text(value='Datos del Paciente', color='red')],
+                alignment=ft.MainAxisAlignment.START,
+                expand=True
+            ),
+            main_content()
+        ],
+        expand=True
+    )
