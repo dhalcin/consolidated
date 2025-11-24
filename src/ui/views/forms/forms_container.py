dd12935d15504f3a0d432624a09c285d24ab241f
jhon <hankgit-h@hotmail.com>
2025-11-24 03:57:04 -0500

    feat(forms_container): import the patient_form_container module module

--- src/ui/views/forms/forms_container.py
+++ src/ui/views/forms/forms_container.py
@@ -2,6 +2,7 @@ import flet as ft
 from src.ui.views.forms.sections import build_file_picker_container
 from src.ui.views.forms.sections import build_general_data_container
 from src.ui.views.forms.sections import build_establishment_container
+from src.ui.views.forms.sections import build_patient_form_container
 
 def build_forms_container():
     return ft.Column(
@@ -27,6 +28,13 @@ def build_forms_container():
                         content=build_establishment_container(),
                         padding=5,
                         col={"sm": 4, 'xl': 3},
+                        border=ft.border.all(1, '#36B7E2'),
+                        border_radius=ft.border_radius.all(10)
+                    ),
+                    ft.Container(
+                        content=build_patient_form_container(),
+                        padding=5,
+                        col={'sm': 4, 'xl': 3},
                         border=ft.border.all(1, '#36B7E2'),
                         border_radius=ft.border_radius.all(10)
                     )
