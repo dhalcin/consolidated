8a584efd4900bfd7e3dd28be2c974d0740b46a7d
jhon <hankgit-h@hotmail.com>
2025-11-24 04:26:46 -0500

    feat(forms_container): import the professional_data_container module

--- src/ui/views/forms/forms_container.py
+++ src/ui/views/forms/forms_container.py
@@ -3,6 +3,7 @@ from src.ui.views.forms.sections import build_file_picker_container
 from src.ui.views.forms.sections import build_general_data_container
 from src.ui.views.forms.sections import build_establishment_container
 from src.ui.views.forms.sections import build_patient_form_container
+from src.ui.views.forms.sections import build_professional_data_container
 
 def build_forms_container():
     return ft.Column(
@@ -33,6 +34,13 @@ def build_forms_container():
                     ),
                     ft.Container(
                         content=build_patient_form_container(),
+                        padding=5,
+                        col={'sm': 4, 'xl': 3},
+                        border=ft.border.all(1, '#36B7E2'),
+                        border_radius=ft.border_radius.all(10)
+                    ),
+                    ft.Container(
+                        content=build_professional_data_container(),
                         padding=5,
                         col={'sm': 4, 'xl': 3},
                         border=ft.border.all(1, '#36B7E2'),
