3e44aba7c34766b8bdb2b662a72eeecbac749065
jhon <hankgit-h@hotmail.com>
2025-11-25 11:33:21 -0500

    style(file_picker): add a simple style for the open file button

--- src/ui/views/forms/sections/file_picker_container.py
+++ src/ui/views/forms/sections/file_picker_container.py
@@ -1,15 +1,19 @@
 import flet as ft
 
 def build_file_picker_container():
+    button_style = ft.ButtonStyle(
+        bgcolor={
+            ft.ControlState.DEFAULT: '#36B7E2',
+            ft.ControlState.HOVERED: ft.Colors.GREEN_ACCENT_400
+        },
+        color={ft.ControlState.DEFAULT: ft.Colors.WHITE},
+        shape={ft.ControlState.DEFAULT: ft.RoundedRectangleBorder(radius=9)}
+    )
+    
     button = ft.ElevatedButton(
-        text='Pick Files',
-        icon=ft.Icons.UPLOAD_FILE,
-        bgcolor='#36B7E2',
-        color=ft.Colors.WHITE,
+       text='Open File',
+       icon=ft.Icons.UPLOAD_FILE,
+        style=button_style
     )
 
-    return ft.Column(
-        [
-            button
-        ]
-    )
+    return ft.Column([button])
