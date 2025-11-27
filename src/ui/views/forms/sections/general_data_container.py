9f05f5720fc33609c81e05b44c61870d7f7c8b68
jhon <hankgit-h@hotmail.com>
2025-11-27 14:51:21 -0500

    refactor(general_data): simplify dropdown creation and remove repeated code

--- src/ui/views/forms/sections/general_data_container.py
+++ src/ui/views/forms/sections/general_data_container.py
@@ -1,65 +1,48 @@
 import flet as ft
 
-def main_content():
+def create_options(list_options):
+    options = []
 
-    example_data = [
-        'L01',
-        'L02',
-        'L03',
-        'L04',
-        'L05'
-    ]
-
-    data = []
-
-    for lt in example_data:
-        data.append(ft.DropdownOption(
-            key=lt,
-            content=ft.Text(lt, color=ft.Colors.BLACK)
+    for op in list_options:
+        options.append(ft.DropdownOption(
+            key=op,
+            content=ft.Text(op, color=ft.Colors.BLACK)
         ))
+
+    return options
+
+def content_label(label):
+    return ft.Text(label, color='black')
+
+def dropdown(label, options):
+    return ft.Dropdown(
+        label=content_label(label),
+        editable=True,
+        options=create_options(options),
+        expand=True,
+        bgcolor=ft.Colors.WHITE
+    ) 
+
+def main_content():
 
     return ft.Column(
         [
             ft.ResponsiveRow(
                 [
-                    ft.Container(
-                        content=ft.Dropdown(
-                            label=ft.Text('# Lote', color='black'),
-                            editable=True,
-                            options=data,
-                            expand=True,
-                            bgcolor=ft.Colors.WHITE,
-                        ),
+                    ft.Container( 
+                        content=dropdown('# Batch', ['L01', 'L02', 'L03']),
                         col={'sm': 6}
                     ),
-                    ft.Container(
-                        content=ft.Dropdown(
-                            label=ft.Text('# Lote', color='black'),
-                            editable=True,
-                            options=data,
-                            expand=True,
-                            bgcolor=ft.Colors.WHITE
-                        ),
+                    ft.Container( 
+                        content=dropdown('# Sheet', ['1', '2', '3']),
                         col={'sm': 6}
                     ),
-                    ft.Container(
-                        content=ft.Dropdown(
-                            label=ft.Text('# Lote', color='black'),
-                            editable=True,
-                            options=data,
-                            expand=True,
-                            bgcolor=ft.Colors.WHITE
-                        ),
+                    ft.Container( 
+                        content=dropdown('# Reg', ['1', '2', '3']),
                         col={'sm': 6}
                     ),
-                    ft.Container(
-                        content=ft.Dropdown(
-                            label=ft.Text('# Lote', color='black'),
-                            editable=True,
-                            options=data,
-                            expand=True,
-                            bgcolor=ft.Colors.WHITE
-                        ),
+                    ft.Container( 
+                        content=dropdown('Date', ['11', '22', '34']),
                         col={'sm': 6}
                     )
                 ]
@@ -72,7 +55,7 @@ def build_general_data_container():
         [
             #! title Data Generales
             ft.Row(
-                [ft.Text(value='Datos Generales', color='red')],
+                [ft.Text(value='General Information', color='red')],
                 alignment=ft.MainAxisAlignment.START,
                 expand=True
             ),
