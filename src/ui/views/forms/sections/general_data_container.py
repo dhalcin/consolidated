f8cd1b563db12f420712f82706162d79e0ae0c79
jhon <hankgit-h@hotmail.com>
2025-11-27 14:26:42 -0500

    feat(general_data): add an initial dropdown

--- src/ui/views/forms/sections/general_data_container.py
+++ src/ui/views/forms/sections/general_data_container.py
@@ -1,31 +1,71 @@
 import flet as ft
 
 def main_content():
+
+    example_data = [
+        'L01',
+        'L02',
+        'L03',
+        'L04',
+        'L05'
+    ]
+
+    data = []
+
+    for lt in example_data:
+        data.append(ft.DropdownOption(
+            key=lt,
+            content=ft.Text(lt, color=ft.Colors.BLACK)
+        ))
+
     return ft.Column(
         [
             ft.ResponsiveRow(
                 [
                     ft.Container(
-                        content=ft.Text(value='N° de Lote', color='red'),
+                        content=ft.Dropdown(
+                            label=ft.Text('# Lote', color='black'),
+                            editable=True,
+                            options=data,
+                            expand=True,
+                            bgcolor=ft.Colors.WHITE,
+                        ),
                         col={'sm': 6}
                     ),
                     ft.Container(
-                        content=ft.Text(value='N° de Hoja', color='red'),
+                        content=ft.Dropdown(
+                            label=ft.Text('# Lote', color='black'),
+                            editable=True,
+                            options=data,
+                            expand=True,
+                            bgcolor=ft.Colors.WHITE
+                        ),
                         col={'sm': 6}
                     ),
                     ft.Container(
-                        content=ft.Text(value='N° de Registro', color='red'),
+                        content=ft.Dropdown(
+                            label=ft.Text('# Lote', color='black'),
+                            editable=True,
+                            options=data,
+                            expand=True,
+                            bgcolor=ft.Colors.WHITE
+                        ),
                         col={'sm': 6}
                     ),
                     ft.Container(
-                        content=ft.Text(value='Fecha', color='red'),
+                        content=ft.Dropdown(
+                            label=ft.Text('# Lote', color='black'),
+                            editable=True,
+                            options=data,
+                            expand=True,
+                            bgcolor=ft.Colors.WHITE
+                        ),
                         col={'sm': 6}
                     )
                 ]
             )
         ]
     )
-
 
 def build_general_data_container():
     return ft.Column(
